c3ac09601f3bdd425a9184def9420f0f50b72801
sadid <sadid.chowdhury64@gmail.com>
2025-03-11 01:54:53 +0600

    some css changs in searchbar and Hero

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'> 
 
         <h1 className='md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto'>Empower your future with the courses designed to 
           <span className='text-blue-600'> fit your choice. </span> <img src={assets.sketch} alt="sketch"className='md:block hidden absolute -bottom-7 right-0' />
--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,30 +1,30 @@
-import React, { use, useState } from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
-const SearchBar = ({data}) => {
+const SearchBar = ({ data }) => {
+  const navigate = useNavigate() // to navigate to the search page or other pages
 
-const navigate= useNavigate()     //to navigate to the search page or other pages
+  // to store the input field data entered in the search bar
+  const [input, setInput] = useState(data ? data : '')
 
+  const onSearchHandler = (e) => {
+    e.preventDefault() // when we submit the form, the page should not reload
+    navigate('/course-list/' + input) // navigate to the course-list page with the input
+  }
 
-//to store the input field data entered in the search bar
-const [input, setInput] = useState(data ? data : '')
-
-const onSearchHandler = (e) => {
-    e.preventDefault()                //when we submit the form, the page should not reload
-    navigate('/course-list/' + input)   //navigate to the course-list page with the input
-}
-
-
-return (
-  
-        <form onSubmit={onSearchHandler} className='max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded'>
-            <img src={assets.search_icon} alt="search icon" className='md:w-auto w-10 px-3' />
-            <input onChange={(e)=> setInput(e.target.value)} value={input} 
-           type="text" placeholder='Search for courses' className='w-full h-full outline-none text-grey-500/80' /> 
-           {/*value={input} is used to display the input field data entered in the search bar*/}
+  return (
+    <form onSubmit={onSearchHandler} className='max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded'>
+      <img src={assets.search_icon} alt="search icon" className='md:w-auto w-10 px-3' />
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+        type="text"
+        placeholder='Search for courses'
+        className='w-full h-full outline-none text-grey-500/80'
+      />
            
-            <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'> Search </button>
+            <button type='submit'  className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1 transition-transform duration-300 hover:bg-blue-700 hover:shadow-lg hover:scale-105'> Search </button> 
         </form>
    
 )
